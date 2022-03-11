1245a20b7f32b16a5c1084449fd39829d99000c9
jibminJung <jibminjung@gmail.com>
2022-03-11 21:42:57 +0900

    Multi line print

---
+++ src/P5337/Main.kt
@@ -0,0 +1,13 @@
+package P5337
+
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    print(
+        """.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/."""
+    )
+}
