dd36f23ed1030560423055fecf5806e1e1e35ad4
jibminJung <jibminjung@gmail.com>
2022-04-05 00:02:47 +0900

    Implementation

--- src/P1292/Main.kt
+++ src/P1292/Main.kt
@@ -6,16 +6,15 @@ import java.io.InputStreamReader
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val (a,b) = readLine().split(" ").map { it.toInt() }
     val arr = IntArray(1001)
+    var i = 1
+    var k = 0
     loop@while (true){
-        var i = 1
-        var k = 0
         for (j in 1..i){
             if(k+j>1000) break@loop
             arr[k+j] = i
         }
         k += i
         i++
-
     }
     var sum = 0
     for (i in a..b){
