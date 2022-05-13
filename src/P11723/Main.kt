29d22db862fe6e51bcc48598d03c5b37eeea7747
jibminJung <jibminjung@gmail.com>
2022-05-13 20:49:21 +0900

    BitMasking with infix fun

--- src/P11723/Main.kt
+++ src/P11723/Main.kt
@@ -2,25 +2,33 @@ package P11723
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.util.*
 
 val sb = StringBuilder()
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var full = 0L
-    for (i in 1..20) {
-        full = full or (1L shl i)
-    }
+fun main()=with(BufferedReader(InputStreamReader(System.`in`))){
     var set = 0L
     repeat(readLine().toInt()) {
-        val st = StringTokenizer(readLine())
-        when (st.nextToken()) {
-            "add" -> set = set or (1L shl st.nextToken().toInt())
-            "remove" -> set = set and (1L shl st.nextToken().toInt()).inv()
-            "check" -> sb.append(if((set and (1L shl st.nextToken().toInt()))>1L) 1 else 0).append("\n")
-            "toggle" -> set = set xor (1L shl st.nextToken().toInt())
-            "all" -> set = set or full
+        val order = readLine().split(" ")
+        val n = order.last().toIntOrNull()?:0
+        when (order.first()) {
+            "add" -> set = set on n
+            "remove" -> set = set off n
+            "check" -> sb.append(set chk n).append("\n")
+            "toggle" -> set = set toggle n
+            "all" -> set = set or 0L.inv()
             "empty" -> set = 0L
         }
     }
     println(sb)
 }
+
+infix fun Int.on(i:Int) = this or (1 shl i-1)
+infix fun Long.on(i:Int) = this or (1L shl i-1)
+
+infix fun Int.off(i:Int) = this and (1 shl i-1).inv()
+infix fun Long.off(i:Int) = this and (1L shl i-1).inv()
+
+infix fun Int.chk(i:Int) = if(this and (1 shl i-1)>=1) 1 else 0
+infix fun Long.chk(i:Int) = if(this and (1L shl i-1)>=1L) 1 else 0
+
+infix fun Int.toggle(i:Int) = this xor (1 shl i-1)
+infix fun Long.toggle(i:Int) = this xor (1L shl i-1)
