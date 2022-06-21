0562d63d260a7393b293cf1bfa680d18981e685e
jibminJung <jibminjung@gmail.com>
2022-06-22 00:39:14 +0900

    HashSet

--- src/P1269/Main.kt
+++ src/P1269/Main.kt
@@ -6,6 +6,9 @@ import java.io.InputStreamReader
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     fun input() = readLine().split(" ").map { it.toInt() }
     val (a,b) = input()
-    val inter = input().toSet().intersect(input()).count()
-    println(a-inter+b-inter)
+    val aSet = input().toHashSet()
+    val bSet = input().toHashSet()
+    val subSet1 = aSet-bSet
+    val subSet2 = bSet-aSet
+    println(subSet1.size+subSet2.size)
 }
